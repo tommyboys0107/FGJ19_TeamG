9d49ac4d069a8d66d77e226b6929a5a41b3c42ce
Regulus89 <regulus890729@gmail.com>
2019-08-24 17:29:15 +0800

    小女孩會走直線惹~~~

---
+++ Assets/Scripts/EdgTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgTrigger : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Girl")
+        {
+            other.GetComponent<GirlMove>().isCorner = true;
+            print(other.GetComponent<GirlMove>().isCorner);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Girl")
+        {
+            other.GetComponent<GirlMove>().isCorner = false;
+            print(other.GetComponent<GirlMove>().isCorner);
+        }
+    }
+}
---
+++ Assets/Scripts/GirlMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GirlMove : MonoBehaviour
+{
+    bool isWait;
+    public bool isCorner;
+    Rigidbody rb;
+    [SerializeField]
+    float speed = 10;
+
+    System.Random crandom = new System.Random();
+    int r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isWait = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWait == false)
+        {
+            //random
+            r = crandom.Next(0, 2);
+            isWait = true;
+            if (r == 0) {
+                GoUp();
+                print(r);
+            }
+            else if (r == 1)
+            {
+                GoDown();
+                print(r);
+            }
+
+        }
+    }
+    
+    void GoUp()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.forward  * speed);
+        StartCoroutine(Wait(1f));
+    }
+    void GoDown()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.forward * -speed);
+        StartCoroutine(Wait(1f));
+    }
+
+    IEnumerator Wait(float n) {
+        yield return new WaitForSeconds(n);
+        isWait = false;
+    }
+}
