94ae30311b7c1a288ccd06e42ea4ec182695ef38
Regulus89 <regulus890729@gmail.com>
2019-08-24 19:35:21 +0800

    Shake

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Shake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.DOShakePosition(1.0f,0.5f,999);
+    }
+}
