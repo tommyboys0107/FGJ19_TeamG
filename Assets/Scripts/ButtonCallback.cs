1562ace080a64c897054042fbb903ec37b2b35cc
moom <age39303@gmail.com>
2019-08-25 10:31:58 +0800

    modify menu UI & change Game Manager

--- Assets/Scripts/ButtonCallback.cs
+++ Assets/Scripts/ButtonCallback.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class ButtonCallback:MonoBehaviour
 {
+    [SerializeField] private UnityEvent Onstart;
+
+    void Start ()
+    {
+        Onstart.Invoke();
+    }
+
+    //btn callback
     public void startButtonCallback ()
     {
         SceneManager.LoadScene( 1 );
@@ -18,7 +28,7 @@ public class ButtonCallback:MonoBehaviour
         SceneManager.LoadScene( 1 );
         GameManager.gameState = GameManager.GameState.Playing;
     }
-    public void menuButtonCallback()
+    public void menuButtonCallback ()
     {
         SceneManager.LoadScene( 0 );
         GameManager.gameState = GameManager.GameState.Menu;
