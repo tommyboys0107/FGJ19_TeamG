abbffa6c09084e6bd0f52095e17443584dce1375
moom <age39303@gmail.com>
2019-08-25 15:02:21 +0800

    menuUI

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuUI : MonoBehaviour
+{
+    public GameObject playImage;
+
+    private void Update ()
+    {
+        switch ( GameManager.gameState )
+        {
+            case GameManager.GameState.Menu:
+                GameObject btnSelected = GameObject.Find( EventSystem.current.currentSelectedGameObject.name );
+                playImage.transform.position = btnSelected.transform.position + new Vector3( -( btnSelected.transform.position.x * 5 / 10 ), 0, 0 );
+                break;
+        }
+    }
+}
